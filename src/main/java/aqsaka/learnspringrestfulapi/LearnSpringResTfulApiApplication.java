d9911b758b865f43a15c612977d35741156d2e92
alaqsaka <aqsaabidin15@gmail.com>
2023-07-20 20:53:05 +0700

    first commit

---
+++ src/main/java/aqsaka/learnspringrestfulapi/LearnSpringResTfulApiApplication.java
@@ -0,0 +1,12 @@
+package aqsaka.learnspringrestfulapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringResTfulApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringResTfulApiApplication.class, args);
+	}
+}
