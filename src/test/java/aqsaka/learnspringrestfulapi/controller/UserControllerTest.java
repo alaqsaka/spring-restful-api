3a335098654bb1419627b8349845260a87e4be63
alaqsaka <aqsaabidin15@gmail.com>
2023-07-21 18:59:56 +0700

    Create unit test for register success, edit application properties for showing sql statment when running unit test

--- src/test/java/aqsaka/learnspringrestfulapi/controller/UserControllerTest.java
+++ src/test/java/aqsaka/learnspringrestfulapi/controller/UserControllerTest.java
@@ -1,15 +1,24 @@
 package aqsaka.learnspringrestfulapi.controller;
 
+import aqsaka.learnspringrestfulapi.model.RegisterUserRequest;
+import aqsaka.learnspringrestfulapi.model.WebResponse;
 import aqsaka.learnspringrestfulapi.repository.UserRepository;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.MockMvcBuilder.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -22,13 +31,35 @@ class UserControllerTest {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @BeforeEach
     void setUp() {
         userRepository.deleteAll();
     }
 
     @Test
-    void testRegisterSuccess() {
+    void testRegisterSuccess() throws Exception {
+        RegisterUserRequest request = new RegisterUserRequest();
+        request.setUsername("test");
+        request.setPassword("password");
+        request.setName("Test");
 
+        mockMvc.perform(
+                post("/api/users")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request))
+        ).andExpectAll(
+            status().isOk()
+        ).andDo( result -> {
+            WebResponse<String> response = objectMapper.readValue(result.getResponse().getContentAsString()
+                    , new TypeReference<>() {
+                    });
+
+            assertEquals("OK", response.getData());
+                }
+        );
     }
 }
